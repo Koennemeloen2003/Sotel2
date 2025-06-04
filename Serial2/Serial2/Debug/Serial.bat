4410c59565a8cda1f34d18eca2151c25568c326d
Koennemeloen2003 <kaben1901@gmail.com>
2025-06-04 12:11:44 +0200

    first

---
+++ Serial2/Serial2/Debug/Serial.bat
@@ -0,0 +1,1 @@
+C:\Users\kaben\Downloads\avrdude-v7.1-windows-windows-x64\avrdude -p atmega328p -c arduino -P com16 -b 57600 -D -D -U flash:w:Serial2.hex
---
+++ Serial2/Serial2/main.c
@@ -0,0 +1,114 @@
+/*
+ * Serial2.c
+ *
+ * Created: 20/05/2025 11:07:42
+ * Author : kaben
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/sfr_defs.h>
+#include <avr/io.h>
+
+typedef unsigned char byte;
+
+inline void serial_write_byte(byte c)
+{
+	loop_until_bit_is_set(UCSR0A, UDRE0); UDR0 = c;
+}
+
+inline byte serial_read_byte()
+{
+	loop_until_bit_is_set(UCSR0A, RXC0); return UDR0;
+}
+
+byte serial_read_nibble()
+{
+	byte nibble = serial_read_byte();
+	
+	if ((nibble >= 'A') && (nibble <= 'F'))
+	{
+		nibble -= 'A' - 10;
+	}
+	else if ((nibble >= 'a') && (nibble <= 'f'))
+	{
+		nibble -= 'a' - 10;
+	}
+	else if ((nibble >= '0') && (nibble <= '9'))
+	{
+		nibble -= '0';
+	}
+	else
+	{
+		nibble = 0;
+	}
+	
+	return nibble;
+}
+
+
+int main(void)
+{
+
+	UBRR0L = 0X10;
+	UBRR0H = 0;
+	
+	UCSR0B = (1 << RXEN0)|(1 << TXEN0); // receive & transmit enabled
+	
+	UCSR0C = 3 << UCSZ00; // 8 data-bits, 1 stop-bit, no parity
+	
+	DDRB = 0xFF;
+	DDRC = 0xFF;
+	DDRD = 0;
+	
+	PORTB =0;
+	PORTC = 0;
+	PORTD = 0;
+	
+	while (1)
+	{
+		byte input = serial_read_byte();
+		
+		if (input == 'B')
+		{
+			byte hi = serial_read_nibble();
+			byte lo = serial_read_nibble();
+			
+			PORTB = (hi << 4) | lo;
+			
+			serial_write_byte('.');
+		}
+		else if (input == 'A'){
+			byte hi = serial_read_nibble();
+			byte lo = serial_read_nibble();
+			
+			shift_out_595((hi << 4) | lo);
+			serial_write_byte('!');
+		}
+		else
+		{
+			serial_write_byte('?');
+		}
+	}
+}
+
+void shift_out_595(uint8_t data) {
+	data = ~data;
+	for (int i = 7; i >= 0; i--) {
+		// Set DATA bit
+		if (data & (1 << i))
+		PORTC |= (1 << PC0);   // DATA high
+		else
+		PORTC &= ~(1<<PC0);  // DATA low
+
+		// Pulse CLOCK
+		PORTC |= (1<<PC1);
+		PORTC &= ~(1<<PC1);
+	}
+
+	// Pulse LATCH
+	PORTC |=(1<<PC2);
+	PORTC &=~(1<<PC2);
+	PORTC &= ~(1<<PC0);
+}
+
